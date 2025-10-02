966037862333edafcd6d65559018b2ae77633234
Threemarine <84086266+tldbs0420@users.noreply.github.com>
2025-10-02 12:32:15 +0900

    [UPDATE] Updated GaussJordan
    가우스-조던 방식의 역행렬 구하기 완료

---
+++ GetInverse.py
@@ -0,0 +1,130 @@
+
+def GetInverseByDeterminant(M):
+    return 0
+
+# 가우스-조던 소거법으로 역행렬 계산
+def GetInverseByGaussJordan(M):
+    M2 = [row[:] for row in M]  # 원본 유지를 위한 deep copy
+    n = len(M2[0])
+
+    # 역행렬이 될 단위행렬 I
+    I = [[float(0) for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        I[i][i] = 1
+    
+    for i in range(n):
+        # 주 대각 성분에 접근했는데, 0이면 계산할 수 없음
+        # 그러면 본인의 아래쪽 행들과 비교해서 계산할 수 있는 행이 있다면 해당 행과 교체
+        if (M2[i][i] == 0):
+            swap = -1
+            for k in range(i+1, n):
+                if (M2[k][i] != 0):
+                    swap = k
+                    break
+            if (swap == -1):    # 바꿀 수 있는 행이 없음 : 역행렬이 없음
+                print("역행렬이 없습니다.")
+                return None
+            M2[i], M2[swap] = M2[swap], M2[i]
+            I[i], I[swap] = I[swap], I[i]
+        # 피벗을 기준으로 다른 행 계산
+        for j in range(n):
+            if (i == j): continue
+            ratio = M2[j][i]/M2[i][i]   # 피벗(주 대각 성분 i, i)을 기준으로 본인 아랫쪽의 행과의 비율 계산
+            for k in range(n): 
+                M2[j][k] = M2[j][k] - ratio * M2[i][k]  # 계산한 비율을 토대로 모든 열 연산
+                I[j][k] = I[j][k] - ratio * I[i][k]     # 역행렬이 될 단위 행렬에도 똑같은 비율을 적용하여 계산
+    
+    # 주 대각성분 1로 만들기
+    for i in range(n):
+        divisor = M2[i][i]
+        for j in range(n):
+            I[i][j] = I[i][j]/divisor   # 원래의 행렬은 굳이 계산할 필요가 없다.
+    
+    return I    # 역행렬 반환
+
+
+# 행렬 출력
+def PrintMatrix(M):
+    n = len(M[0])
+    if n == 1 : 
+        # print("[ %7.3f ]" %M[0][0])   # 이렇게 하니까 부동소수점 오차로 인해 0이여야 할 것이 -0.000으로 출력됨
+        print("[ %7.3f ]" %CleanRound(M[0][0], 3))
+        return
+
+    print("┌ ", end = "")
+    for i in range(n):
+        print("%7.3f " %CleanRound(M[0][i], 3), end = "")
+    print("  ┐")
+
+    for x in range(1, n-1):
+        print("│ ",  end = "")
+        for y in range(n):
+            print("%7.3f " %CleanRound(M[x][y], 3),  end = "")
+        print("  │")
+
+    print("└ ", end = "")
+    for i in range(n):
+        print("%7.3f " %CleanRound(M[n-1][i]),  end = "")
+    print("  ┘")
+
+# 부동 소수점 오차로 인해 -0이 출력되는 문제를 해결
+def CleanRound(num, r=3):
+    eps=1e-9
+    num = round(num, r)
+    if abs(num) < eps:
+        return 0.0
+    return num
+
+def main():
+    while True:
+        while True: # 몇 차원 행렬로 할 것인지 입력
+            try:
+                n = int(input("역행렬을 계산할 정방 행렬의 차수 (-1 To Exit) : "))
+                if (n < 1 and n != -1):
+                    print("숫자를 양수로 입력해주세요.")
+                    continue
+                break
+            except:
+                print("숫자를 제대로 입력해주세요.")
+        if (n == -1):
+            break
+
+        # n * n 행렬 생성 및 0으로 채워넣기
+        M = [[float(0) for _ in range(n)] for _ in range(n)]
+
+        inputNumbers = []
+        while len(inputNumbers) < n * n:
+            # 숫자 입력 (띄어쓰기 구분 가능)
+            inputLine = input("숫자 입력 : ").split()
+            numbersFromLine = []
+            # 입력받은 줄 내에서 해석 가능 범위까지 해석
+            try :
+                for num in inputLine:
+                    numbersFromLine.append(float(num))
+            except:
+                print("숫자를 제대로 입력해주세요.")
+
+            # 해석한 숫자들을 하나씩 추가
+            try:
+                for num in numbersFromLine:
+                    inputNumbers.append(num)
+            except:
+                print("숫자를 제대로 입력해주세요.")
+
+        # 숫자 할당
+        for i in range(n):
+            for j in range(n):
+                M[i][j] = inputNumbers[i * n + j]
+
+        print("입력한 행렬 : ")
+        PrintMatrix(M)
+
+        # 가우스-조던 소거법으로 역행렬 계산
+        GJ = GetInverseByGaussJordan(M)
+        if (GJ == None): continue
+
+        print("가우스-조던 소거법으로 계산한 역행렬 : ")
+        PrintMatrix(GJ)
+
+if __name__ == "__main__":
+    main()
